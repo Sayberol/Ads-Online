d1244d11d64a038a72f3cd697215486fbbd8f622
saybe <Sayberol>
2022-05-29 22:01:45 +0500

    &&

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,24 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
 from users.managers import UserManager
-# from phonenumber_field.modelfields import PhoneNumberField
-# from django.utils.translation import gettext_lazy as _
-
-
-# class UserRoles(models.Model):
-#     USER = 'user'
-#     ADMIN = 'admin'
-#     ROLES = [
-#         ("user", "Пользователь"),
-#         ("admin", "Админ")
-#     ]
-#
-#     first_name = models.CharField(max_length=20)
-#     last_name = models.CharField(max_length=20)
-#     phone = models.CharField(max_length=15)
-#     email = models.CharField(User.USERNAME_FIELD, max_length=10)
-#     role = models.CharField(max_length=9, choices=ROLES, default="user")
-#     image = models.ImageField(upload_to='ads/', null=True, blank=True)
 
 
 class User(AbstractBaseUser):
